650fb8d9678b183d1b7bb5e4393ec8fcfcc20ac6
Edward Saviano <lyonz4prez@gmail.com>
2023-09-27 15:03:58 +0800

    homepage & connect to styles

---
+++ views/homepage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Card } from 'react-native-elements';
+import styles from './styles/styles.js';
+
+export default function HomePage() {
+  return (
+    <View style={styles.container}>
+      
+      {/* Navigation Bar */}
+      <View style={styles.navBar}>
+        <TouchableOpacity style={styles.navButton}>
+          <Text>Nav 1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navButton}>
+          <Text>Previous Quote</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.navButton}>
+          <Text>New Quote</Text>
+        </TouchableOpacity>
+      </View>
+      
+      {/* Quote Card */}
+      <Card containerStyle={styles.quoteCard}>
+        <Card.Title>“The obstacle is the way.”</Card.Title>
+        <Text style={styles.authorText}>- Marcus Aurelius</Text>
+      </Card>
+      
+    </View>
+  );
+}
