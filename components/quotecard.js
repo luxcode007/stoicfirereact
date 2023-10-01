32be4a394fac0303cc832b606c21ec0f94ec2fe5
Edward Saviano <lyonz4prez@gmail.com>
2023-10-01 17:54:53 +0700

    update quotecard with import doc, getdoc for sdk 10.4

--- components/quotecard.js
+++ components/quotecard.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { db } from '../firebaseConfig.js';
+import { doc, getDoc } from 'firebase/firestore';
 import { Text } from 'react-native';
 import { Card } from 'react-native-elements';
 import styles from '../styles/styles';
-import db from '../firebaseConfig.js';  // Adjust the path
 
 export default function QuoteCard() {
     const [quote, setQuote] = useState('');
@@ -10,14 +11,19 @@ export default function QuoteCard() {
 
     useEffect(() => {
         const fetchData = async () => {
-        try {
-            const doc = await db.collection('quotes').doc('stoicquote00').get();
-            const data = doc.data();
-            setQuote(data.quote);
-            setAuthor(data.author);
-        } catch (error) {
-            console.log('Error getting document:', error);
-        }
+            try {
+                const docRef = doc(db, 'quotes', 'stoicquote00'); // Creating a reference to the specific document
+                const docSnap = await getDoc(docRef); // Fetching the document using the reference
+
+                if (docSnap.exists()) { // Checking if document exists
+                    setQuote(docSnap.data().quote); // Setting quote state with fetched data
+                    setAuthor(docSnap.data().author); // Setting author state with fetched data
+                } else {
+                    console.log('No such document!');
+                }
+            } catch (error) {
+                console.log('Error getting document:', error);
+            }
         };
 
         fetchData();
