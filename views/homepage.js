86bc476524c2a284257c311ca01c42feadff37d8
Edward Saviano <lyonz4prez@gmail.com>
2023-10-01 12:51:40 +0700

    quotecard db component, undefined error needs fix

--- views/homepage.js
+++ views/homepage.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Card } from 'react-native-elements';
+// import { Card } from 'react-native-elements'; now unnecessary per new card componenet below
 import styles from '../styles/styles';
+import QuoteCard from '../components/quotecard';
 
 export default function HomePage() {
-  return (
-    <View style={styles.container}>
-      
-      {/* Navigation Bar */}
-      <View style={styles.navBar}>
-        <TouchableOpacity style={styles.navButton}>
-          <Text>Nav 1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.navButton}>
-          <Text>Previous Quote</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.navButton}>
-          <Text>New Quote</Text>
-        </TouchableOpacity>
-      </View>
-      
-      {/* Quote Card */}
-      <Card containerStyle={styles.quoteCard}>
-        <Card.Title>“The obstacle is the way.”</Card.Title>
-        <Text style={styles.authorText}>- Marcus Aurelius</Text>
-      </Card>
-      
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+        
+        {/* Navigation Bar */}
+        <View style={styles.navBar}>
+            <TouchableOpacity style={styles.navButton}>
+            <Text>Nav 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navButton}>
+            <Text>Previous Quote</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.navButton}>
+            <Text>New Quote</Text>
+            </TouchableOpacity>
+        </View>
+        
+        {/* OLD in-view Quote Card */}
+        {/* <Card containerStyle={styles.quoteCard}>
+            <Card.Title>“The obstacle is the way.”</Card.Title>
+            <Text style={styles.authorText}>- Marcus Aurelius</Text>
+        </Card> */}
+
+        {/* NEW component Quote Card, connected to firestore db */}
+        <QuoteCard />
+        
+        </View>
+    );
 }
