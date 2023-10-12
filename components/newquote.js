3fb81fc39a2d4ae583fa1f0b5729385e5fb418c7
Edward Saviano <lyonz4prez@gmail.com>
2023-10-12 20:11:49 +0700

    newquote & prevquote logic & db added

--- components/newquote.js
+++ components/newquote.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Button } from 'react-native';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
 
-function RandomQuoteButton({ setQuote, setAuthor }) {
+function RandomQuoteButton({ setQuote, setAuthor, setPreviousQuote }) {
     const fetchRandomQuote = async () => {
         // Logic to fetch a random quote from Firestore
         // Once fetched, you can use setQuote and setAuthor to update the state in the parent component
+        const randomIndex = Math.floor(Math.random() * 10); // Replace YOUR_QUOTE_COUNT with actual count
+        const quoteDoc = await getDoc(doc(db, 'quotes', `quote${randomIndex}`));
+        if (quoteDoc.exists()) {
+            setPreviousQuote({quote: quote, author: author}); // Set the previous quote before updating to the new one
+            setQuote(quoteDoc.data().quote);
+            setAuthor(quoteDoc.data().author);
+        }
     };
 
     return (
--- components/previousquote.js
+++ components/previousquote.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Button } from 'react-native';
 
-function PreviousQuoteButton({ setQuote, setAuthor }) {
-    const fetchPreviousQuote = async () => {
-        // Logic to fetch the previous quote
-        // Once fetched, use setQuote and setAuthor to update the state in the parent component
+function PreviousQuoteButton({ setQuote, setAuthor, previousQuote }) {
+
+    const goToPreviousQuote = () => {
+        if (previousQuote) {
+            setQuote(previousQuote.quote);
+            setAuthor(previousQuote.author);
+        }
     };
 
     return (
-        <Button title="Previous Quote" onPress={fetchPreviousQuote} />
+        <Button title="Previous Quote" onPress={goToPreviousQuote} />
     );
 }
 
