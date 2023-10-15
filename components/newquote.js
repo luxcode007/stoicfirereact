76b49a76a774b1b58c06f9d7d7a6513bd5a078fa
Edward Saviano <lyonz4prez@gmail.com>
2023-10-15 19:14:10 +0700

    further newquote componenet updates

--- components/newquote.js
+++ components/newquote.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import { getDocs, collection, doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 
 export default function NewQuoteButton({ style, setQuote, setAuthor, setPreviousQuote }) {
+    const [allQuoteIds, setAllQuoteIds] = useState([]);
+    
     useEffect(() => {
         const fetchAllIds = async () => {
             const querySnapshot = await getDocs(collection(db, 'quotes'));
@@ -23,8 +25,11 @@ export default function NewQuoteButton({ style, setQuote, setAuthor, setPrevious
 
             const quoteDoc = await getDoc(doc(db, 'quotes', randomId));
             if (quoteDoc.exists()) {
-                setPreviousQuote({ quote: quote, author: author }); // Set the current quote as previous
-                setQuote(quoteDoc.data().quote);
+                // capturing current state before updating
+                setQuote((currentQuote) => {
+                    setPreviousQuote({ quote: currentQuote, author: author }); // Set the current quote as previous
+                    return quoteDoc.data().quote;
+                });
                 setAuthor(quoteDoc.data().author);
                 console.log('Fetching random quote...');
                 // You may want to communicate this data back to the parent or another component, possibly using a callback.
