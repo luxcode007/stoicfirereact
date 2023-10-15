1b969591ce3d1680ba82092369e2ea4c06de4bc3
Edward Saviano <lyonz4prez@gmail.com>
2023-10-15 18:44:05 +0700

    homepage props & state update

--- views/homepage.js
+++ views/homepage.js
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 // import { Card } from 'react-native-elements'; now unnecessary per new card componenet below
 import styles from '../styles/styles';
 import QuoteCard from '../components/quotecard';
 import NewQuoteButton from '../components/newquote';
 import PreviousQuoteButton from '../components/previousquote';
+import { db } from '../firebaseConfig.js';
+import { doc, getDoc } from 'firebase/firestore';
 
 export default function HomePage() {
     const [quote, setQuote] = useState('');
     const [author, setAuthor] = useState('');
     const [previousQuote, setPreviousQuote] = useState(null);
+
+    useEffect(() => {
+        const fetchInitialQuote = async () => {
+            const docRef = doc(db, 'quotes', 'stoicquote00');
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                setQuote(docSnap.data().quote);
+                setAuthor(docSnap.data().author);
+            } else {
+                console.log('No such document!');
+            }
+        };
+        fetchInitialQuote();
+    }, []);
 
     return (
         <View style={styles.container}>
