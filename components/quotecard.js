9c49d148bfc97147b5eddf5886854c3d38e00978
Edward Saviano <lyonz4prez@gmail.com>
2023-10-15 18:40:12 +0700

    quotecard update

--- components/quotecard.js
+++ components/quotecard.js
@@ -1,33 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { db } from '../firebaseConfig.js';
-import { doc, getDoc } from 'firebase/firestore';
+import React from 'react';
 import { Text } from 'react-native';
 import { Card } from 'react-native-elements';
 import styles from '../styles/styles';
 
-export default function QuoteCard() {
-    const [quote, setQuote] = useState('');
-    const [author, setAuthor] = useState('');
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const docRef = doc(db, 'quotes', 'stoicquote00'); // Creating a reference to the specific document
-                const docSnap = await getDoc(docRef); // Fetching the document using the reference
-
-                if (docSnap.exists()) { // Checking if document exists
-                    setQuote(docSnap.data().quote); // Setting quote state with fetched data
-                    setAuthor(docSnap.data().author); // Setting author state with fetched data
-                } else {
-                    console.log('No such document!');
-                }
-            } catch (error) {
-                console.log('Error getting document:', error);
-            }
-        };
-
-        fetchData();
-    }, []);  // The empty dependency array means this useEffect runs once when the component mounts
+export default function QuoteCard({ quote, author}) {
 
     return (
     <Card containerStyle={styles.quoteCard}>
